766a096f8615b5ecef8bb9008022ad65e085db8e
971023als <971023als@naver.com>
2023-02-09 10:52:02 +0900

    Update U-42.sh

--- U-42.sh
+++ U-42.sh
@@ -20,18 +20,6 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# 다운그레이드 프로세스에 필요한 패키지 설치
-sudo apt-get update
-sudo apt-get install -y dpkg
-
-# 커널 패키지 다운그레이드
-sudo dpkg -i /path/to/downgraded/kernel-package.deb
-
-# Redhat 릴리스 패키지 다운그레이드
-sudo dpkg -i /path/to/downgraded/redhat-release-package.deb
-
-# 시스템을 재부팅하여 변경 사항을 적용합니다
-sudo reboot
 
 
 cat $result
