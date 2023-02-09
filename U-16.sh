4f64be1bd18bec6846054beb43a3115980a7b7ff
971023als <971023als@naver.com>
2023-02-09 12:54:56 +0900

    Update U-16.sh

--- U-16.sh
+++ U-16.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
- 
-
 . function.sh
-
- 
 
 BAR
 
@@ -24,29 +20,27 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-
-
-# Define the backup directory
+# 백업 디렉터리 정의
 backup_dir="/dev_backup"
 
-# Remove the current /dev directory
+# 현재 /dev 디렉토리 제거
 rm -rf /dev
 
-# Check if the removal was successful
+# 제거가 성공적이었는지 확인하십시오
 if [ $? -eq 0 ]; then
-  echo "/dev directory removed successfully"
+  OK "/dev 디렉토리가 제거되었습니다."
 else
-  echo "/dev Directory Removal Failed"
+  INFO "/dev 디렉토리 제거 실패"
 fi
 
-# Copy the backup directory to /dev
-cp -R $backup_dir /dev
+# 백업 디렉토리를 /dev에 복사합니다
+sudo cp -R $backup_dir /dev
 
-# Check if the copy was successful
+# 복사가 성공했는지 확인합니다
 if [ $? -eq 0 ]; then
-  echo "$backup_dir directory copied to /dev successfully"
+  OK "$backup_dir 디렉토리가 /dev에 복사되었습니다."
 else
-  echo "$backup_dir Directory Copy Failed"
+  WARN "$backup_dir 디렉토리 복사 실패"
 fi
  
 cat $result
