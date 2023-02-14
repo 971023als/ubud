ca2835425a371568bce9b76f8258453448e980fb
971023als <971023als@naver.com>
2023-02-15 06:57:07 +0900

    ok

--- U-59.sh
+++ U-59.sh
@@ -23,7 +23,7 @@ EOF
 BAR
 
 # 변수 설정
-backup_dir="/backups/"
+backup_dir="/path/to/backup/dir"
 source_dir="/home/user/"
 
 # 백업 디렉토리(존재하지 않는 경우) 생성
@@ -31,22 +31,20 @@ if [ ! -d "$backup_dir" ]; then
     mkdir "$backup_dir"
 fi
 
-# 숨겨진 파일 백업 (커널 밑에 있는 파일은 제외)
-find / -type f -name ".*" ! -path "/run/user/1000/gvfs/*" ! -path "/sys/*" ! -path "/proc/*" -exec cp --parents {} "$backup_dir" \;
+# 숨겨진 파일 백업
+find / -type f -name ".*" ! -path "/run/user/1000/gvfs/*" -exec cp --parents {} "$backup_dir" \;
 
-# 숨겨진 디렉터리 백업 (커널 밑에 있는 디렉터리는 제외)
-find / -type d -name ".*" ! -path "/run/user/1000/gvfs/*" ! -path "/sys/*" ! -path "/proc/*" -exec cp -r --parents {} "$backup_dir" \;
+# 숨겨진 디렉터리 백업
+find / -type d -name ".*" ! -path "/run/user/1000/gvfs/*" -exec cp -r --parents {} "$backup_dir" \;
 
 #--------------------------------------------------------------------------------------------------
 
 # 변수 설정
-backup_dir="/backups/"
-source_dir="/home/user/"
+backup_dir="/path/to/backup/dir"
+source_dir="/"
 
 # 숨겨진 파일 및 디렉터리 복구
 rsync -a --delete "$backup_dir" "$source_dir"
-
-INFO "숨겨진 디렉토리 및 파일을 복구 완료"
 
 
 cat $result
--- U-60.sh
+++ U-60.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
+ 
+
 . function.sh
+
+  
+ 
 
 BAR
 
 CODE [U-60] ssh 원격접속 허용
 
 cat << EOF >> $result
+
 [양호]: 원격 접속 시 SSH 프로토콜을 사용하는 경우
+
 [취약]: 원격 접속 시 Telnet, FTP 등 안전하지 않은 프로토콜을 사용하는 경우
+
 EOF
 
 BAR
 
+
 TMP1=`SCRIPTNAME`.log
 
 > $TMP1  
+#/etc/telnet
+#/etc/ftp
+#/etc/ssh
 
-# Check if SSH is installed
-if [ ! -f /usr/sbin/sshd ]; then
-echo "SSH is not installed."
 
-# Uninstall SSH
-sudo yum remove ssh -y
 
-# Check if SSH has been successfully uninstalled
-if [ ! -f /usr/sbin/sshd ]; then
-OK "SSH 제거 작업 성공."
-else
-WARN "SSH 제거 작업 실패."
-fi
-else
-INFO "SSH 이미 제거."
-fi
+INFO "이 부분은 백업 파일 관련한 항목이 아닙니다"
+
+#---------------------------------------------------
+
+
+# Stop ssh service
+sudo service ssh stop
+
 
 cat $result
 
-echo ; echo
+echo ; echo 
