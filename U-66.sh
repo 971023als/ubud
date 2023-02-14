a74ae78394c8ef4b876f46b945c2d70a83e8b212
971023als <971023als@naver.com>
2023-02-15 07:01:50 +0900

    ok

--- U-66.sh
+++ U-66.sh
@@ -10,23 +10,28 @@ BAR
 CODE [U-66] SNMP 서비스 구동 점검
 
 cat << EOF >> $result
+
 [양호]: SNMP 서비스를 사용하지 않는 경우
+
 [취약]: SNMP 서비스를 사용하는 경우
+
 EOF
 
 BAR
 
-TMP1=`SCRIPTNAME`.log
-
-> $TMP1
 
 
+INFO "이 부분은 백업 파일 관련한 항목이 아닙니다"
 
-# Start SNMP service
+#---------------------------------------------------
+
+# Start the snmpd service
 sudo service snmpd start
-OK "SNMP 서비스가 시작되었습니다."
 
+INFO "SNMP 서비스를 시작하였습니다."
 
 cat $result
 
 echo ; echo
+ 
+
