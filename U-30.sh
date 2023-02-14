2860dda7a5ed379c9598edc28f6f17248d18d216
971023als <971023als@naver.com>
2023-02-14 21:36:28 +0900

    ok

--- U-30.sh
+++ U-30.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+ 
+
 . function.sh
+
+ 
+TMP1=`SCRIPTNAME`.log
+
+> $TMP1
 
 BAR
 
 CODE [U-30] Sendmail 버전 점검
 
 cat << EOF >> $result
+
 [양호]: Sendmail 버전이 최신버전인 경우 
+
 [취약]: Sendmail 버전이 최신버전이 아닌 경우
+
 EOF
 
 BAR
 
-TMP1=`SCRIPTNAME`.log
+INFO "이 부분은 백업 파일 관련한 항목이 아닙니다"
 
->$TMP1 
+#---------------------------------------------------
 
 # Sendmail 서비스 재시작
 sudo service sendmail restart
@@ -32,3 +42,4 @@ fi
 cat $result
 
 echo ; echo
+ 
