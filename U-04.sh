55713b70e5ba02332e630bd8955871863df0310f
971023als <971023als@naver.com>
2023-02-14 16:30:56 +0900

    ok

--- U-04.sh
+++ U-04.sh
@@ -2,47 +2,82 @@
 
 . function.sh
 
+ 
+ TMP1=`SCRIPTNAME`.log
+
+> $TMP1
+
 BAR
 
 CODE [U-04] 패스워드 파일 보호
 
 cat << EOF >> $result
+
 [양호]: 쉐도우 패스워드를 사용하거나, 패스워드를 암호화하여 저장하는 경우
+
 [취약]: 쉐도우 패스워드를 사용하지 않고, 패스워드를 암호화하여 저장하지 않는 경우
+
 EOF
 
 BAR
 
-TMP1=`SCRIPTNAME`.log
+# 백업할 원본 파일 배열 설정
+files=("/etc/passwd" "/etc/shadow")
 
->$TMP1 
+# 백업 디렉터리 설정
+# backup_dir="/backup"
 
+# 백업 파일의 접두사 설정
+prefix="_backup_"
 
-SHADOW_FILE="/etc/shadow"
-PASSWD_FILE="/etc/passwd"
-
-# 섀도 파일이 있는지 확인하십시오
-if [ -f $SHADOW_FILE ] ; then
-  OK "섀도우 파일이 있습니다."
-
-  # 섀도 파일에 암호화되지 않은 암호가 있는지 확인
-  CHECK=$(cat $PASSWD_FILE | awk -F: '{print $2}' | grep -v 'x')
-  if [ -z $CHECK ] ; then
-    OK "시스템이 암호화된 암호를 사용합니다."
+# 현재 날짜와 시간을 알다
+current_time=$(date +%Y%m%d_%H%M%S)
+for file in "${files[@]}"; do
+  if [ -f "$file" ]; then
+  # 각 원본 파일을 반복합니다
+    # create a new backup file using the current time in the file name
+    cp -p "$file" "$file$prefix$current_time"
+    # 백업이 성공적으로 생성되었음을 나타내는 메시지 표시
+    OK "시스템이 성공적으로 백업되었습니다.: $file$prefix$current_time"
   else
-    INFO "섀도우 파일에서 암호화되지 않은 암호를 암호화하는 중..."
-    # 암호화되지 않은 암호 목록 반복
-    for PASSWORD in $CHECK; do
-      # mkpasswd 유틸리티를 사용하여 암호화
-      ENCRYPTED_PASSWORD=$(mkpasswd -m sha-512 $PASSWORD)
-      # 암호화되지 않은 암호를 섀도 파일의 암호화된 암호로 바꿉니다
-      sed -i "s/$PASSWORD/$ENCRYPTED_PASSWORD/g" $SHADOW_FILE
-    done
-    OK "섀도우 파일에서 암호화되지 않은 암호의 암호화가 완료되었습니다."
+    INFO "$file 을 찾을 수 없습니다"
   fi
-else
-  WARN "섀도우 파일이 없습니다."
-fi
+done
+
+
+# --------------------------------------------------------------------------------------
+
+
+# 원본 파일 배열 설정
+files=("/etc/passwd" "/etc/shadow")
+
+# 백업 디렉터리 설정
+# backup_dir="/backup"
+
+# 백업 파일에 대한 접두사 설정
+prefix="_backup_"
+
+# 각 원본 파일을 반복합니다
+for file in "${files[@]}"; do
+  # 각 원본 파일에 대해 가장 오래된 백업 파일 찾기
+  oldest_backup=$(ls -t "$file$prefix"* | tail -1)
+  if [ -f "$file" ]; then
+    #각 원본 파일에 대해 가장 오래된 백업 파일이 있는지 확인
+    if [ -f "$oldest_backup" ]; then
+      # 가장 오래된 백업 파일을 원래 파일로 복원
+      cp -p "$oldest_backup" "$file"
+      # 복원이 성공했음을 나타내는 메시지를 표시
+      OK "시스템이 성공적으로 원래 상태로 복원되었습니다.: $oldest_backup to $file"
+    else
+      # 가장 오래된 백업 파일이 없음을 나타내는 메시지를 표시
+      WARN "백업 파일을 찾을 수 없습니다. 시스템을 복원할 수 없습니다.: $oldest_backup"
+    fi
+    else
+      INFO "$file 을 찾을 수 없습니다"
+    fi
+done
+
+
 
 cat $result
 
