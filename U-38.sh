c043c93d5998c19c1c74c0f56330f74ba6e58241
971023als <971023als@naver.com>
2023-02-15 10:01:13 +0900

    Update U-38.sh

--- U-38.sh
+++ U-38.sh
@@ -28,11 +28,14 @@ BACKUP_DIR="/root/httpd_backup"
 mkdir -p "$BACKUP_DIR"
 for item in $UNWANTED_ITEMS
 do
-  if [ -f "$HTTPD_ROOT" ]; then
-    cp -r "$HTTPD_ROOT/$item" "$BACKUP_DIR"
-    INFO "$item이 $HTTPD_ROOT에서 $BACKUP_DIR로 백업되었습니다"
+  if [ -f "$HTTPD_ROOT/httpd.conf" ]; then
+    if [ -d "$HTTPD_ROOT/$item" ] || [ -f "$HTTPD_ROOT/$item" ]; then
+      mkdir -p "$BACKUP_DIR/$item"
+      cp -r "$HTTPD_ROOT/$item" "$BACKUP_DIR/$item"
+      INFO "$item이 $HTTPD_ROOT에서 $BACKUP_DIR로 백업되었습니다"
+    fi
   else
-    INFO "$HTTPD_ROOT 을 찾을 수 없습니다"
+    INFO "$HTTPD_ROOT/httpd.conf 을 찾을 수 없습니다"
   fi
 done
 
@@ -46,11 +49,13 @@ BACKUP_DIR="/root/httpd_backup"
 if [ -d "$BACKUP_DIR" ]; then
   for item in $UNWANTED_ITEMS
   do
-    if [ -f "$HTTPD_ROOT" ]; then
-      cp -r "$BACKUP_DIR/$item" "$HTTPD_ROOT"
-      INFO "$item이 $BACKUP_DIR에서 $HTTPD_ROOT로 복원되었습니다."
+    if [ -f "$HTTPD_ROOT/httpd.conf" ]; then
+      if [ -d "$BACKUP_DIR/$item" ] || [ -f "$BACKUP_DIR/$item" ]; then
+        cp -r "$BACKUP_DIR/$item" "$HTTPD_ROOT"
+        INFO "$item이 $BACKUP_DIR에서 $HTTPD_ROOT로 복원되었습니다."
+      fi
     else
-      INFO "$HTTPD_ROOT 을 찾을 수 없습니다"
+      INFO "$HTTPD_ROOT/httpd.conf 을 찾을 수 없습니다"
     fi  
   done
 else
