247c256d2cf1ba0cabaee8b51607abe00f51ce77
971023als <971023als@naver.com>
2023-02-15 11:15:40 +0900

    Update U-38.sh

--- U-38.sh
+++ U-38.sh
@@ -20,39 +20,37 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-HTTPD_ROOT="/etc/apache2/apache2.conf"
-UNWANTED_ITEMS="manual samples docs"
-BACKUP_DIR="/backup_httpd/"
 
-# 원하지 않는 파일 백업
-mkdir -p "$BACKUP_DIR"
-for item in $UNWANTED_ITEMS
-do
-  if [ -d "$HTTPD_ROOT/$item" ] || [ -f "$HTTPD_ROOT/$item" ]; then
-    cp -r "$HTTPD_ROOT/$item" "$BACKUP_DIR"
-    INFO "$item이 $HTTPD_ROOT에서 $BACKUP_DIR로 백업되었습니다"
-  fi
-done
+ACKUP_DIR="/backup_httpd/"
+BACKUP_FILE="apache_backup.tar.gz"
+HTTPD_ROOT="/etc/apache2/"
 
-#------------------------------------------------------------------
+# 백업 
+tar -czvf $BACKUP_DIR$BACKUP_FILE $HTTPD_ROOT
 
-HTTPD_ROOT="/etc/apache2/apache2.conf"
-UNWANTED_ITEMS="manual samples docs"
-BACKUP_DIR="/backup_httpd/"
-
-# 백업 파일 복원
-if [ -d "$BACKUP_DIR" ]; then
-  for item in $UNWANTED_ITEMS
-  do
-    if [ -d "$BACKUP_DIR/$item" ] || [ -f "$BACKUP_DIR/$item" ]; then
-      cp -r "$BACKUP_DIR/$item" "$HTTPD_ROOT"
-      INFO "$item이 $BACKUP_DIR에서 $HTTPD_ROOT로 복원되었습니다."
-    fi
-  done
+# 백업
+if tar -czvf "$BACKUP_DIR$BACKUP_FILE" "$HTTPD_ROOT"; then
+  OK "백업 파일 $BACKUP_FILE이 $BACKUP_DIR에 생성되었습니다."
 else
-  WARN "백업 디렉터리 $BACKUP_DIR이 없습니다."
+  WARN "백업 파일 생성에 실패하였습니다."
 fi
 
+#--------------------------------------------------------------
+
+BACKUP_DIR="/backup_httpd/"
+BACKUP_FILE="apache2_dir.tar.gz"
+HTTPD_ROOT="/etc/apache2/"
+
+# HTTPD_ROOT에 백업 파일 압축 풀기
+tar -xzf $BACKUP_DIR$BACKUP_FILE -C $HTTPD_ROOT
+
+
+# HTTPD_ROOT에 백업 파일 압축 풀기
+if tar -xzf "$BACKUP_DIR$BACKUP_FILE" -C "$HTTPD_ROOT"; then
+  OK "백업 파일 $BACKUP_FILE이 $HTTPD_ROOT에 압축 해제되었습니다."
+else
+  WARN "백업 파일 압축 해제에 실패하였습니다."
+fi
 
 
 cat $result
